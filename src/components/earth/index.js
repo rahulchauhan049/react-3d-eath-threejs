f35784f26b33fbddcf9a337226b28f5354f48877
RAHUL <rahul.chauhan049@gmail.com>
2021-08-11 16:25:20 +0530

    added sun and rotaion

--- src/components/earth/index.js
+++ src/components/earth/index.js
@@ -1,23 +1,41 @@
-import React from "react";
-import { useLoader } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 import * as THREE from "three";
+
 import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
 import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
 import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
 import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
 import { TextureLoader } from "three";
+
 export function Earth(props) {
   const [colorMap, normalMap, specularMap, cloudMap] = useLoader(
     TextureLoader,
     [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
   );
 
+    const earthRef = useRef();
+    const cloudsRef = useRef();
+    useFrame(({ clock }) => {
+        const elapsedTime = clock.getElapsedTime();
+        earthRef.current.rotation.y = elapsedTime / 30;
+        cloudsRef.current.rotation.y = elapsedTime / 28;
+    })
+
   return (
     <>
-          <ambientLight intensity={0.5} />
-          <Stars radius={300} depth={60} count={20000} factor={7} saturation={0} fade={true} />
-      <mesh>
+      {/* <ambientLight intensity={0.5} /> */}
+      <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={1.2} />
+      <Stars
+        radius={300}
+        depth={60}
+        count={20000}
+        factor={7}
+        saturation={0}
+        fade={true}
+      />
+      <mesh ref={cloudsRef}>
         <sphereGeometry args={[1.005, 32, 32]} />
         <meshPhongMaterial
           map={cloudMap}
@@ -27,10 +45,15 @@ export function Earth(props) {
           side={THREE.DoubleSide}
         />
       </mesh>
-      <mesh>
+      <mesh ref={earthRef}>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
-        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          metalness={0.4}
+          roughness={0.7}
+        />
         <OrbitControls
           enableZoom={true}
           enablePan={true}
